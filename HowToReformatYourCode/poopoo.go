2d7d333b940079aaacf1f4bd4372dab03db5154a
Mizumoto <mizumotokunn@gmail.com>
2022-05-25 11:02:33 +0900

    init/gofmt for format

---
+++ HowToReformatYourCode/poopoo.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() { fmt.Printf("some bullshit %s", "poopoo") }
+
+// reformatted from poopoo.go.bak using gofmt tool
---
+++ HowToReformatYourCode/reformat.bat
@@ -0,0 +1,1 @@
+gofmt -w ./poopoo.go
